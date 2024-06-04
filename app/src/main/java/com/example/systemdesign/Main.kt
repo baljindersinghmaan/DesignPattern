0cbd2d1baa9839bcb00f6a9c8b91a742b68f29e3
baljinder <baljindermaan15@gmail.com>
2024-06-04 19:10:32 -0400

    code cleanup

--- app/src/main/java/com/example/systemdesign/Main.kt
+++ app/src/main/java/com/example/systemdesign/Main.kt
@@ -3,38 +3,11 @@ package com.example.systemdesign
 fun main(){
     val bicycleFactory = SimpleBicycleFactory()
     try {
-        val bikeToBuild = bicycleFactory.createBicycle("Motion")
-        bikeToBuild.build()
+        bicycleFactory.createBicycle("Motion")
+
     } catch (e: Exception) {
-        println(errorText)
+        println(e)
 
     }
-
-}
-class Main {
-
-    /*fun build() {
-        println("Manufacturing a ${geometry} frame...")
-        buildStatus = ManufacturingStatus.FrameManufactured
-        printBuildStatus()
-
-        println("Painting the frame ${color}")
-        buildStatus = ManufacturingStatus.Painted
-        printBuildStatus()
-
-        if (suspension != SuspensionTypes.Hardtail) {
-            println("Mounting the ${suspension} suspension.")
-            buildStatus = ManufacturingStatus.SuspensionMounted
-            printBuildStatus()
-        }
-
-        println("${LocalDate.now().year} $modelName Bicycle serial number $serialNumber manufacturing complete!")
-        buildStatus = ManufacturingStatus.Complete
-        printBuildStatus()
-    }*/
-
-    /*private fun printBuildStatus() {
-        println("Current build status: $buildStatus")
-    }*/
 
 }
